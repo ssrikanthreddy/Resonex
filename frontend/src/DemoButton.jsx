74789cafb60588384b2fde262436f09bc5ed8144
Tanmay Adithya <marnitanmayadithya@gmail.com>
2023-10-28 11:54:37 +0530

    Revert "refactor: Add React Router and format with Prettier"
    This reverts commit cb9b33bfeabae25d41d4c25a3629e9287acbf05e.

--- frontend/src/DemoButton.jsx
+++ frontend/src/DemoButton.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import gsap from 'gsap';
 
 const DemoButton = () => {
@@ -7,23 +7,25 @@ const DemoButton = () => {
     // This function will be executed when the button is clicked
   };
 
-  useEffect(() => {
-    setTimeout(() => {
-      const demoButton = document.querySelectorAll('.demo-button');
+  useEffect(()=> {
 
-      gsap.fromTo(
-        demoButton,
-        { opacity: 0 },
-        { opacity: 1, duration: 2, stagger: 0.01, ease: 'power2.out' }
-      );
+    setTimeout(()=>{
+        const demoButton = document.querySelectorAll('.demo-button');
 
-      gsap.fromTo(
-        demoButton,
-        { y: '40px' },
-        { y: '0%', duration: 0.4, stagger: 0.01, ease: 'power2.out' }
-      );
-    }, 2500);
-  }, []);
+        gsap.fromTo(
+            demoButton,
+            { opacity: 0 },
+            { opacity: 1, duration: 2, stagger: 0.01, ease: 'power2.out' }
+        );
+
+        gsap.fromTo(
+            demoButton,
+            { y: '40px' },
+            { y: '0%', duration: 0.4, stagger: 0.01, ease: 'power2.out' }
+        );
+
+    }, 2500)
+  }, [])
 
   return (
     <button className="demo-button" onClick={handleClick}>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import LandingPage from './LandingPage.jsx';
-import Dashboard from './Dashboard.jsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <LandingPage />,
-  },
-  {
-    path: '/dashboard',
-    element: <Dashboard />,
-  },
-]);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import LandingPage from './LandingPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+    <LandingPage />
+  </React.StrictMode>,
+)
