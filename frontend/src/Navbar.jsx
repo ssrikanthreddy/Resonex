b4fc50fc3ed0b6c74371a1ee5b3a5d90627bdebe
Tanmay Adithya <marnitanmayadithya@gmail.com>
2023-11-03 19:17:39 +0530

    style: Change Demo to GitHub in Nav

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -6,24 +6,24 @@ const Navbar = () => {
   const [isTop, setIsTop] = useState(true);
 
   useEffect(() => {
-    setTimeout(()=>{
+    setTimeout(() => {
       const navs = document.querySelectorAll('.nav-elements, nav');
 
-    gsap.fromTo(
-      navs,
-      { opacity: 0 },
-      { opacity: 1, duration: 1, stagger: 0.01, ease: 'power2.out' }
-    );
+      gsap.fromTo(
+        navs,
+        { opacity: 0 },
+        { opacity: 1, duration: 1, stagger: 0.01, ease: 'power2.out' }
+      );
 
-    gsap.fromTo(
-      navs,
-      { y: '40px' },
-      { y: '0%', duration: 0.4, stagger: 0.01, ease: 'power2.out' }
-    );
-    }, 2000)
+      gsap.fromTo(
+        navs,
+        { y: '40px' },
+        { y: '0%', duration: 0.4, stagger: 0.01, ease: 'power2.out' }
+      );
+    }, 2000);
 
     const handleScroll = () => {
-      if (window.scrollY > 500) {
+      if (window.scrollY > 300) {
         setIsTop(false);
       } else {
         setIsTop(true);
@@ -38,8 +38,8 @@ const Navbar = () => {
   }, []);
 
   const navbarStyles = {
-    opacity: isTop? "1": "0",
-    transition: "opacity 0.4s"
+    opacity: isTop ? '1' : '0',
+    transition: 'opacity 0.4s',
   };
 
   const navCenterStyles = {
@@ -48,10 +48,10 @@ const Navbar = () => {
   };
 
   const logoStyles = {
-    opacity: isTop? "1": "0",
+    opacity: isTop ? '1' : '0',
     height: '80px',
     width: '80px',
-    transition: "opacity 0.4s"
+    transition: 'opacity 0.4s',
   };
   return (
     <nav>
@@ -84,7 +84,21 @@ const Navbar = () => {
       </div>
       <div className="nav-elements nav-right" style={navbarStyles}>
         <button className="btn gradient-border">
-          <Link to="/dashboard">Demo</Link>
+          <a
+            href="https://github.com/ssrikanthreddy/reactserver"
+            rel="noreferrer"
+            target="_blank"
+          >
+            GitHub
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="1em"
+              viewBox="0 0 512 512"
+              style={{ fill: '#f6fdc3', marginLeft: '5px' }}
+            >
+              <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
+            </svg>
+          </a>
         </button>
       </div>
     </nav>
